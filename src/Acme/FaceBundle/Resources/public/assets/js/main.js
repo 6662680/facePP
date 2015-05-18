d12bea7c337250ce8c7ca6492b00aa1985d25cf8
LatteCake <wack1001@163.com>
2015-05-18 10:36:03 +0800

    change upload image url

--- src/Acme/FaceBundle/Resources/public/assets/js/main.js
+++ src/Acme/FaceBundle/Resources/public/assets/js/main.js
@@ -93,7 +93,7 @@ var main = {
             var faceTooltipWidth  = Math.ceil(imageWidth * (positoin.width / 100)),
                 faceTooltipHeight = Math.ceil(imageHeight * (positoin.height / 100)),
                 faceTooltipLeft   = Math.ceil(imageWidth * (positoin.center.x / 100) - (faceTooltipWidth / 2)),
-                faceTooltipTop    = Math.ceil(imageWidth * (positoin.center.y / 100) / 2 - ( positoin.center.y / 2 ) ),
+                faceTooltipTop    = Math.ceil(imageWidth * (positoin.center.y / 100) / 2 - ( faceTooltipHeight / 2 ) ),
                 tooltipTop        = Math.ceil(faceTooltipTop - (faceTooltipHeight) / 2) - 49,
                 tooltipLeft       = Math.ceil(faceTooltipLeft + (faceTooltipWidth / 2)) - 42;
 
